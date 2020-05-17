f41fb198d7cadbe61139bdb80d3eca1a4b8c9a2a
9sako6 <31821663+9sako6@users.noreply.github.com>
2020-05-17 18:19:10 +0900

    fix: sample

--- sample/sample.ts
+++ sample/sample.ts
@@ -1,5 +1,4 @@
-// import TsGitHubProjects from 'ts-github-projects';
-import TsGitHubProjects from '../src/TsGitHubProjects';
+import TsGitHubProjects from '../src/index';
 require('dotenv').config();
 const gh = new TsGitHubProjects({ token: process.env.PERSONAL_ACCESS_TOKEN! });
 
@@ -7,7 +6,6 @@ async function demo() {
   // rate limit
   const info = await gh.rateLimit();
   console.log(info);
-  console.log(await gh.listRepositoryProjects('9sako6', 'ts-github-projects', 'closed'))
 }
 
 demo();
